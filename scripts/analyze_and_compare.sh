c893155bb5c65f92360afe78b22373910de91dbd
termitaklk <112192503+termitaklk@users.noreply.github.com>
2024-09-01 14:31:15 -0400

    sd

--- scripts/analyze_and_compare.sh
+++ scripts/analyze_and_compare.sh
@@ -56,7 +56,7 @@ done
 
 # Si hay nuevas listas, añadirlas a la línea de listas
 if [ ! -z "$NEW_LISTS" ]; then
-    sed -i "${LIST_LINE}s/$/${NEW_LISTS}/" "$LFLIST_FILE"
+    sed -i -e "${LIST_LINE}s/$/${NEW_LISTS}/" "$LFLIST_FILE"
 fi
 
 # Clonar el repositorio de destino directamente en la raíz
