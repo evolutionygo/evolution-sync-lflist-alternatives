1fbc7360bf2eeb82cb4900376751c5265f19d681
termitaklk <112192503+termitaklk@users.noreply.github.com>
2024-09-01 15:51:19 -0400

    ja

--- scripts/analyze_and_compare.sh
+++ scripts/analyze_and_compare.sh
@@ -7,7 +7,8 @@ DEST_REPO_URL="https://${TOKEN}@github.com/termitaklk/koishi-Iflist.git"
 DEST_REPO_DIR="koishi-Iflist"
 
 # Verificar que el archivo lflist.conf existe
-if [ ! -f "$LFLIST_FILE" ]; entonces echo "Error: No se encontró el archivo $LFLIST_FILE"
+if [ ! -f "$LFLIST_FILE" ]; then
+    echo "Error: No se encontró el archivo $LFLIST_FILE"
     exit 1
 fi
 
@@ -24,16 +25,17 @@ NEW_CONTENT=""
 
 # Iterar sobre todos los archivos .conf en el repositorio de comparación
 for conf_file in comparison-repo/*.conf; do
-    if [ -f "$conf_file" ]; entonces
+    if [ -f "$conf_file" ]; then
         # Extraer la lista del archivo .conf actual que está en el formato #[nombre]
         ITEM=$(grep -oP '^#\[\K[^\]]+' "$conf_file")
 
-        if [ -z "$ITEM" ]; entonces echo "No se encontró una lista válida en $conf_file" >> $OUTPUT_FILE
+        if [ -z "$ITEM" ]; then
+            echo "No se encontró una lista válida en $conf_file" >> $OUTPUT_FILE
             continue
         fi
 
         # Verificar si el item ya está en la lista inicial
-        if echo "$INITIAL_LISTS" | grep -q "\[$ITEM\]"; entonces
+        if echo "$INITIAL_LISTS" | grep -q "\[$ITEM\]"; then
             echo "$ITEM de $conf_file ya se encuentra en la lista inicial" >> $OUTPUT_FILE
         else
             echo "$ITEM de $conf_file NO se encuentra en la lista inicial. Añadiendo..." >> $OUTPUT_FILE
@@ -45,13 +47,13 @@ for conf_file in comparison-repo/*.conf; do
 done
 
 # Añadir las nuevas listas al final de la línea inicial
-if [ ! -z "$NEW_LISTS" ]; entonces
+if [ ! -z "$NEW_LISTS" ]; then
     UPDATED_LISTS="${INITIAL_LISTS}${NEW_LISTS}"
     sed -i "1s|.*|${UPDATED_LISTS}|" "$LFLIST_FILE"
 fi
 
 # Añadir el contenido de las nuevas listas al final del archivo
-if [ ! -z "$NEW_CONTENT" ]; entonces
+if [ ! -z "$NEW_CONTENT" ]; then
     echo -e "$NEW_CONTENT" >> "$LFLIST_FILE"
 fi
 
@@ -65,7 +67,8 @@ tail -n 20 "$LFLIST_FILE"  # Mostrar las últimas 20 líneas para verificar el c
 
 # Clonar el repositorio de destino
 git clone "$DEST_REPO_URL" "$DEST_REPO_DIR"
-if [ $? -ne 0 ]; entonces echo "Error: No se pudo clonar el repositorio de destino."
+if [ $? -ne 0 ]; then
+    echo "Error: No se pudo clonar el repositorio de destino."
     exit 1
 fi
 
@@ -81,6 +84,7 @@ git config user.email "action@github.com"
 git add "$LFLIST_FILE"
 git commit -m "Add updated lflist.conf"
 git push origin main
+
 
 
 
